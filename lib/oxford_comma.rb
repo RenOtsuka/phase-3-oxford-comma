b3c92a61fcde3944735a49a1442bb852240c3e0d
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-06-22 14:22:23 -0500

    commit tree 912213703fe35158d865751ca0ac86737fb4b8bb

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,16 @@
 def oxford_comma(array)
-
+    result = []
+    last = array[-1]
+    case array.length
+    when 1
+        array[0]
+    when 2
+        array.join(" and ")
+    else
+        result = array.take(array.length - 1)
+        result << "and "
+        return_arr = result.join(", ")
+        return_arr.split(" ")
+        return_arr << last
+    end
 end
